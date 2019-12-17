501cb3a662b33af00a088f75f1892cc39d2a3e6e
Pokerznik-O <57524527+Pokerznik-O@users.noreply.github.com>
2019-12-17 21:22:17 +0100

    Create Oliver2.py

---
+++ Oliver2.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+rng = np.random.RandomState(42)
+x = 10 * rng.rand(500)
+y = 4*x**3 + 1 + 10*rng.randn(500)
+plt.scatter(x, y)
+#plt.plot(x, 2*x**2 + 10*x - 1)
+
+def F(x, y, a, c):
+    e = 0.
+    for i in range(len(y)):
+        e = e + (y[i] - ((a*x**3)[i] + c))**2
+    return e/(2*(len(y)))
+
+F(x, y, 4, 1)
+
+def derivative(x, y, a, c):
+    f = y - (a*x**3 + c)
+    dfa = f*(-x**3)
+    dfc = f*(-1)
+    return np.sum(dfa)/len(y), np.sum(dfc)/len(y)
+
+print(derivative(x, y, 4, 1))
+#print((F(x, y, 2.0001, 10, -1) - F(x, y, 2, 10, -1))/0.0001) 
+#print((F(x, y, 2, 10.0001, -1) - F(x, y, 2, 10, -1))/0.0001)
+#print((F(x, y, 2, 10, -0.9999) - F(x, y, 2, 10, -1))/0.0001)
+
+def gradient_descent(x, y, a, c):
+    function = []
+    for i in range(100000):
+        df = derivative(x, y, a, c)
+        a = a - 0.00001*df[0] 
+        c = c - 0.00001*df[1]
+        f = a*x**3 + c
+        function.append(f)
+    return round(a), round(c), function
+
+solution = gradient_descent(x, y, 4, 1)
+y2 = solution[0]*(x**3) + solution[1]
+
+plt.scatter(x, y2)
